c0709b6d28543e6bc63595ce4e60a64555993e7a
AnnaBerdik <Anneta-Polakova@yandex.ru>
2024-02-01 17:55:59 +0300

    JS

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,32 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+// @todo: Темплейт карточки
+
+
+const cardTemplate = document.querySelector('#card-template').content;
+const placesList = document.querySelector('.places__list');
+
+function createCard(element, deleteCard) {
+  const cardItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardItem.querySelector('.card__title').textContent = element.name;
+  cardItem.querySelector('.card__image').src = element.link;
+
+  const buttonRemove = cardItem.querySelector('.card__delete-button');
+  buttonRemove.addEventListener('click', function () {
+    deleteCard(cardItem);
+  });
+
+  return cardItem;
+}
+
+initialCards.forEach(function (element) {
+  const cardItem = createCard(element, deleteCard);
+  placesList.prepend(cardItem);
+});
+
+function deleteCard(cardItem) {
+  cardItem.remove();
+}
+
